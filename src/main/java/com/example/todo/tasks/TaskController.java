182ecfdb6710b8dd4cbe00ad25f106445e04edef
pulkit <pulkitsoni2001@gmail.com>
2023-01-19 15:50:00 +0530

    error handling

--- src/main/java/com/example/todo/tasks/TaskController.java
+++ src/main/java/com/example/todo/tasks/TaskController.java
@@ -1,6 +1,9 @@
 package com.example.todo.tasks;
 
+import com.example.todo.common.ErrorResponseDto;
 import jakarta.websocket.server.PathParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,28 +15,58 @@ import java.util.List;
 public class TaskController {
 
     private TaskService taskService;
-    public TaskController(TaskService taskService){
+
+    public TaskController(TaskService taskService) {
         this.taskService = taskService;
     }
+
     @GetMapping("")
-    ResponseEntity<List<TaskDto>> getAllTask(){
+    ResponseEntity<List<TaskDto>> getAllTask() {
         var tasks = taskService.getAllTasks();
         return ResponseEntity.ok(tasks);
     }
 
     @PostMapping("")
-    ResponseEntity<TaskDto> createNewTask(@RequestBody TaskDto taskDto){
+    ResponseEntity<TaskDto> createNewTask(@RequestBody TaskDto taskDto) {
         var savedTask = taskService.createNewTask(taskDto);
-        return ResponseEntity.created(URI.create("/task"+ savedTask.getId())).body(savedTask);
+        return ResponseEntity.created(URI.create("/task" + savedTask.getId())).body(savedTask);
     }
 
     @GetMapping("/{id}")
-    ResponseEntity<TaskDto> getTaskById(@PathVariable Long id){
+    ResponseEntity<TaskDto> getTaskById(@PathVariable Long id) {
         var task = taskService.getTaskId(id);
         return ResponseEntity.ok(task);
     }
 
     @PatchMapping("/{id}")
-    void updateTaskById(){}
+    void updateTaskById() {
+    }
 
+    @ExceptionHandler({
+            TaskService.TaskNotFoundxception.class,
+            TaskService.TaskAlreadyExistsException.class,
+            TaskService.TaskInvalidException.class,
+    })
+    ResponseEntity<ErrorResponseDto> handleError(Exception e){
+        if (e instanceof TaskService.TaskNotFoundxception){
+            return ResponseEntity
+                    .status(HttpStatus.NOT_FOUND)
+                    .body(new ErrorResponseDto(e.getMessage()));
+        }
+        if (e instanceof TaskService.TaskAlreadyExistsException){
+            return ResponseEntity
+                    .status(HttpStatus.CONFLICT)
+                    .body(new ErrorResponseDto(e.getMessage()));
+        }
+        if (e instanceof TaskService.TaskInvalidException){
+            return ResponseEntity
+                    .status(HttpStatus.BAD_REQUEST)
+                    .body(new ErrorResponseDto(e.getMessage()));
+
+        }
+        return ResponseEntity
+                .status(HttpStatus.INTERNAL_SERVER_ERROR)
+                .body(new ErrorResponseDto(e.getMessage()));
+
+    }
 }
--- src/main/java/com/example/todo/tasks/TaskService.java
+++ src/main/java/com/example/todo/tasks/TaskService.java
@@ -4,6 +4,7 @@ package com.example.todo.tasks;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
+import java.util.Date;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -25,14 +26,35 @@ public class TaskService {
     }
 
     public TaskDto getTaskId(Long id){
-        var task  = taskRespository.findById(id).orElse(null);
+        var task  = taskRespository.findById(id).orElseThrow(() -> new TaskNotFoundxception(id));
         return modelMapper.map(task,TaskDto.class);
     }
 
     public TaskDto createNewTask(TaskDto task){
+        if(task.getDueDate() != null && task.getDueDate().before(new Date())){
+            throw new TaskInvalidException("due date must in the future");
+        }
         var taskEntity = modelMapper.map(task,TaskEntity.class);
         var saveTask =  taskRespository.save(taskEntity);
         return modelMapper.map(saveTask,TaskDto.class);
+    }
+
+    static class TaskNotFoundxception extends IllegalArgumentException{
+        public TaskNotFoundxception(Long id){
+            super("task with id "+ id + "not found");
+        }
+    }
+
+    static class TaskAlreadyExistsException extends IllegalArgumentException{
+        public TaskAlreadyExistsException(Long id){
+            super("task with id "+ id + "is already exists");
+        }
+    }
+
+    static class TaskInvalidException extends IllegalArgumentException{
+        public TaskInvalidException(String message){
+            super(message);
+        }
     }
 
 }
